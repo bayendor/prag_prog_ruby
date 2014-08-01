71bf9569b47930a859618ff2867b7103b1e835ce
David Bayendor <dbayendor@gmail.com>
2014-08-01 07:11:10 -0600

    Bonus project

---
+++ crowdfund.rb
@@ -0,0 +1,10 @@
+project1 = 'ABC'
+funding1 = 1000
+project2 = 'LMN'
+funding2 = 1000
+project3 = 'XYZ'
+funding3 = 1000
+
+puts "Project #{project1} has $#{funding1} in funding."
+
+puts "Projects:\n \t#{project1}\n\t#{project2}\n\t#{project3}"

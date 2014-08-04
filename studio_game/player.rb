3c47accda2ec8f3a766fe1504746638c09ea3050
David Bayendor <dbayendor@gmail.com>
2014-08-03 19:35:41 -0600

    Add die rolls and conditional game play
    * Refactor code

--- studio_game/player.rb
+++ studio_game/player.rb
@@ -30,6 +30,10 @@ class Player
     current_time.strftime('%I:%M:%S')
   end
 
+  def strong?
+    @health > 100
+  end
+
   def to_s
     "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end

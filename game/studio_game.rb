bc5b51f9d08bac21bd57cd1878d9338c31b4c9a4
David Bayendor <dbayendor@gmail.com>
2014-08-13 12:21:44 -0600

    Remove commented out code

--- game/studio_game.rb
+++ game/studio_game.rb
@@ -1,15 +1,8 @@
 require_relative 'game'
 require_relative 'player'
 
-# player1 = Player.new('moe')
-# player2 = Player.new('larry', 60)
-# player3 = Player.new('curly', 125)
-#
 knuckleheads = Game.new('Knuckleheads')
 knuckleheads.load_players(ARGV.shift || 'players.csv')
-# knuckleheads.add_player(player1)
-# knuckleheads.add_player(player2)
-# knuckleheads.add_player(player3)
 
 loop do
   puts "How many game rounds? ('quit' to exit)"
